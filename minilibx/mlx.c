a9037f3bb828fc17f430b34abd13023f7dfc354e
Beatriz Dile <bedos-sa@student.42sp.org.br>
2023-09-21 10:46:48 -0300

    print sprite

--- minilibx/mlx.c
+++ minilibx/mlx.c
@@ -8,11 +8,17 @@
 
 # define WINDOW_WIDTH 600
 # define WINDOW_HEIGHT 300
+# define COWBOY "cowboy_front_64.xpm"
 
 typedef struct s_data
 {
     void	*mlx_ptr;
     void	*win_ptr;
+    void	*img;
+    int	    img_width;
+    int	    img_height;
+    int     x;
+    int     y;
 }	t_data;
 
 int	handle_no_event(void *data)
@@ -32,7 +38,10 @@ int	handle_keypress(int keysym, t_data *data)
 int	handle_keyrelease(int keysym, t_data *data)
 {
 	if (keysym == XK_Escape)
+    {
+        mlx_destroy_image(data->mlx_ptr, data->img);
         mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+    }
 
     printf("Keyrelease: %d\n", keysym);
     return (0);
@@ -41,6 +50,7 @@ int	handle_keyrelease(int keysym, t_data *data)
 int	handle_x_press(t_data *data)
 {
     mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+    mlx_destroy_image(data->mlx_ptr, data->img);
     return (0);
 }
 
@@ -57,6 +67,11 @@ int	main(void)
         free(data.win_ptr);
         return (1);
     }
+
+    data.x = 0;
+    data.y = 0;
+    data.img = mlx_xpm_file_to_image(data.mlx_ptr, COWBOY, &data.img_width, &data.img_height);
+    mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img, data.x, data.y);
 
     /* Setup hooks */ 
     mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data); // call a function at the end of every loop
